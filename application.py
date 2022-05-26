3e3b0d43323cdc7a67ed2575b0945fbc238abb0e
BhavuAnand <bhawna135btit20@igdtuw.ac.in>
2022-05-26 15:42:41 +0530

    second commit

--- application.py
+++ application.py
@@ -1,31 +1,22 @@
 import pandas as pd
 import pickle
 import streamlit as st
-
-
 def recommend(products):
     products_index = prod[prod['productDisplayName'] == products].index[0]
     distances = similarity[products_index]
     products_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-
     recommended_products = []
     for i in products_list:
         recommended_products.append(prod.iloc[i[0]].productDisplayName)
         recommended_products.append(prod.iloc[i[0]].homepage)
     return recommended_products
-
-
 products_dict = pickle.load(open('product_dict.pkl', 'rb'))
-
 prod = pd.DataFrame(products_dict)
-
 similarity = pickle.load(open('similarity.pkl', 'rb'))
-
 st.title('PHARMACY RECOMMENDS')
 selected_product_name = st.selectbox(
     'Enter the products you need...',
     prod['productDisplayName'].values)
-
 if st.button('Recommend Me'):
     recommendations = recommend(selected_product_name)
     for i in recommendations:
